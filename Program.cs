0eb4ec9a7c5b9c95817ef542c89963e23ee1b75a
Ethan Nguyen <etnguyen16@gmail.com>
2026-01-11 22:34:18 -0700

    Add system and disk diagnostics

--- Program.cs
+++ Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("PC Diagnostic Tool v0.1");
+        Console.WriteLine("------------------------");
+
+        PrintBasicSystemInfo();
+        PrintDiskInfo();
+
+        Console.WriteLine("\nScan complete.");
+    }
+
+    static void PrintBasicSystemInfo()
+    {
+        Console.WriteLine("\n== System Info ==");
+
+        Console.WriteLine($"OS: {RuntimeInformation.OSDescription}");
+        Console.WriteLine($"Architecture: {RuntimeInformation.OSArchitecture}");
+        Console.WriteLine($"Machine Name: {Environment.MachineName}");
+        Console.WriteLine($"User: {Environment.UserName}");
+        Console.WriteLine($"CPU Cores: {Environment.ProcessorCount}");
+        Console.WriteLine($".NET Version: {Environment.Version}");
+    }
+
+    static void PrintDiskInfo()
+    {
+        Console.WriteLine("\n== Disk Info ==");
+
+        foreach (var drive in DriveInfo.GetDrives())
+        {
+            if (!drive.IsReady) continue;
+
+            Console.WriteLine($"Drive: {drive.Name}");
+            Console.WriteLine($"  Format: {drive.DriveFormat}");
+            Console.WriteLine($"  Total: {ToGb(drive.TotalSize)} GB");
+            Console.WriteLine($"  Free:  {ToGb(drive.AvailableFreeSpace)} GB");
+        }
+    }
+
+    static long ToGb(long bytes) => bytes / 1024 / 1024 / 1024;
+}
